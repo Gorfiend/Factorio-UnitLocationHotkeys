c228bb46cf6f8122a75179e5f4c195763656f945
Gorfiend <Gorfiend@gmail.com>
2022-09-10 17:04:25 -0400

    Add support for entity targets, picking spidertron remote

--- control.lua
+++ control.lua
@@ -59,12 +59,14 @@ local function rebuild_table(player_data)
         end
         table.add {
             type = "sprite-button",
-            tooltip = prefix .. "Click to go to this position\nRight-click to edit",
+            tooltip = prefix .. [["Click to go to this position/entity
+                    - Hold Control to pick a remote if the target is a spidertron
+                    Right-click to edit
+                    - Hold Alt to only edit the target position/entity"]],
             sprite = slot.sprite,
             number = index,
             tags = {
                 cls_action = "go_to_location_button",
-                data = slot,
                 index = index,
             }
         }
@@ -101,24 +103,75 @@ local function init_player(player)
 end
 
 --- @param player LuaPlayer
-local function add_shortcut(player)
+--- @param selection EventData.on_player_selected_area?
+local function add_shortcut(player, selection)
     local player_data = global.players[player.index]
+
 
     --- @type ConfigSlot
     local config_slot = {}
     -- would really like to get the current player camera zoom/position, but can't...
     config_slot.zoom = zoom_constants.world_min
     config_slot.mode = defines.render_mode.chart_zoomed_in
-    config_slot.position = player.position
-    config_slot.sprite = "item/radar"
-    config_slot.caption = "x=" .. player.position.x .. " y=" .. player.position.y
+
+
+    if selection then
+        if #(selection.entities) > 0 then
+            --- @type LuaEntity
+            local entity = selection.entities[1]
+            config_slot.entity = entity
+            config_slot.sprite = "entity/" .. entity.name
+            config_slot.caption = entity.entity_label or entity.name
+        end
+    else
+        local position = player.position
+        if selection then
+            position = selection.area.left_top
+        end
+        config_slot.position = position
+        config_slot.sprite = "item/radar"
+        config_slot.caption = "x=" .. position.x .. " y=" .. position.y
+    end
+
+
     table.insert(player_data.config, config_slot)
     rebuild_table(player_data)
 end
 
 --- @param player LuaPlayer
+--- @param entity LuaEntity
+local function do_pick_remote(player, entity)
+    if entity.type ~= "spider-vehicle" then return end
+    if not player.cursor_stack then return end
+    if player.cursor_stack.connected_entity == entity then return end
+    if not player.clear_cursor() then return end
+
+    local inv = player.get_main_inventory()
+    -- Try to get a connected remote from the inventory
+    if inv and inv.get_item_count("spidertron-remote") then
+        for i = 1, #inv do
+            local stack = inv[i]
+            if stack.connected_entity == entity then
+                player.cursor_stack.swap_stack(stack)
+                player.hand_location = { inventory = inv.index, slot = i --[[@as uint]] }
+                return
+            end
+        end
+    end
+    -- otherwise create one
+    -- TODO add a map option for requiring you to have a remote in your inventory
+    if player.is_cursor_empty() then
+        player.cursor_stack.set_stack { name = "cls-spidertron-remote-oic", count = 1 }
+        player.cursor_stack.connected_entity = entity
+    end
+end
+
+--- @param player LuaPlayer
 --- @param data ConfigSlot
-local function go_to_location(player, data)
+--- @param pick_remote boolean?
+local function go_to_location(player, data, pick_remote)
+    pick_remote = pick_remote or false
+    --- @type MapPosition
     local position
     if data.position then
         position = data.position
@@ -128,20 +181,39 @@ local function go_to_location(player, data)
             return
         end
         position = data.entity.position
+        if pick_remote then
+            do_pick_remote(player, data.entity)
+        end
     end
+    if position == nil then return end -- shouldn't happen...
 
     if data.zoom < zoom_constants.world_min then
-        player.open_map(data.position, data.zoom)
+        player.open_map(position, data.zoom)
     else
-        player.zoom_to_world(data.position, data.zoom)
+        player.zoom_to_world(position, data.zoom)
     end
 end
 
+--- @param player LuaPlayer
+--- @param index integer
+--- @param pick_remote boolean?
+local function go_to_location_index(player, index, pick_remote)
+    go_to_location(player, global.players[player.index].config[index], pick_remote)
+end
+
 --- @param player_data PlayerData
+--- @return ConfigSlot
 local function get_editing_slot(player_data)
     return player_data.config[player_data.gui.edit_window.slot_index]
 end
 
+---@param position MapPosition
+---@return string
+local function position_to_string(position)
+    return "x=" .. position.x .. " y=" .. position.y
+end
+
+---@param player LuaPlayer
 local function refresh_edit_window(player)
     local player_data = global.players[player.index]
     local edit_window_data = player_data.gui.edit_window
@@ -150,7 +222,7 @@ local function refresh_edit_window(player)
     local slot_data = get_editing_slot(player_data)
 
 
-    edit_window_data.name_field.text = slot_data.caption
+    edit_window_data.name_field.text = slot_data.caption or ""
 
     local _, _, sprite_type, sprite_name = string.find(slot_data.sprite, "(.*)/(.*)")
     if sprite_type == "entity" then
@@ -159,6 +231,23 @@ local function refresh_edit_window(player)
     elseif sprite_type == "recipe" then
         edit_window_data.entity_button.elem_value = nil
         edit_window_data.recipe_button.elem_value = sprite_name
+    else
+        edit_window_data.entity_button.elem_value = nil
+        edit_window_data.recipe_button.elem_value = nil
+    end
+
+    if slot_data.position then
+        edit_window_data.location_label.caption = position_to_string(slot_data.position)
+    elseif slot_data.entity then
+        if slot_data.entity.entity_label then
+            edit_window_data.location_label.caption = slot_data.entity.entity_label ..
+                " (" .. position_to_string(slot_data.entity.position) .. ")"
+        else
+            edit_window_data.location_label.caption = slot_data.entity.name ..
+                " (" .. position_to_string(slot_data.entity.position) .. ")"
+        end
+    else
+        edit_window_data.location_label = nil
     end
 
     edit_window_data.zoom_field.text = tostring(slot_data.zoom)
@@ -216,6 +305,7 @@ local function open_edit_window(player, slot_index)
         type = "textfield",
         name = "cls_edit_window_name_field",
     }
+    edit_window_data.name_field.style.natural_width = 300
 
 
     controls_table.add {
@@ -248,6 +338,22 @@ local function open_edit_window(player, slot_index)
 
     controls_table.add {
         type = "label",
+        caption = "Location",
+    }
+    local location_flow = controls_table.add {
+        type = "flow",
+    }
+    edit_window_data.location_label = location_flow.add {
+        type = "label",
+    }
+    edit_window_data.location_button = location_flow.add {
+        type = "button",
+        name = "cls_edit_window_location_button",
+        caption = "Change..."
+    }
+
+    controls_table.add {
+        type = "label",
         caption = "Zoom",
     }
     local zoom_flow = controls_table.add {
@@ -272,15 +378,29 @@ local function open_edit_window(player, slot_index)
 end
 
 -- GUI Event handlers
+
 script.on_event(defines.events.on_gui_click, function(e)
     if e.element.name == "cls_add_shortcut_button" then
-        -- if e.shift...
-        add_shortcut(game.players[e.player_index])
+        if e.shift then
+            add_shortcut(game.players[e.player_index])
+        else
+            local player = game.players[e.player_index]
+            local player_data = global.players[e.player_index]
+            if not player.cursor_stack then return end
+            close_edit_window(player_data)
+            player.clear_cursor()
+            player.cursor_stack.set_stack { name = "cls-location-selection-tool", count = 1 }
+        end
+    elseif e.element.name == "cls_edit_window_location_button" then
+        local player = game.players[e.player_index]
+        if not player.cursor_stack then return end
+        player.clear_cursor()
+        player.cursor_stack.set_stack { name = "cls-location-selection-tool", count = 1 }
     elseif e.element.tags.cls_action == "go_to_location_button" then
         if e.button == defines.mouse_button_type.left then
-            go_to_location(game.players[e.player_index], e.element.tags.data--[[@as ConfigSlot]] )
+            go_to_location_index(game.players[e.player_index], e.element.tags.index--[[@as number]] , e.control)
         else
-            open_edit_window(game.players[e.player_index], e.element.tags.index--[[@as number]])
+            open_edit_window(game.players[e.player_index], e.element.tags.index--[[@as number]] )
         end
     end
 end)
@@ -327,10 +447,47 @@ script.on_event(defines.events.on_gui_elem_changed, function(e)
     end
 end)
 
-script.on_event(defines.events.on_gui_closed, function(event)
-    if event.element and event.element.name == "cls_edit_window_frame" then
-        close_edit_window(global.players[event.player_index])
+script.on_event(defines.events.on_gui_closed, function(e)
+    if e.element and e.element.name == "cls_edit_window_frame" then
+        close_edit_window(global.players[e.player_index])
     end
+end)
+
+---@param e EventData.on_player_selected_area|EventData.on_player_alt_selected_area
+local function on_selection(e)
+    local player_index = e.player_index
+    if e.item ~= "cls-location-selection-tool" or not player_index then return end
+    local player = game.players[player_index]
+    local player_data = global.players[player_index]
+    if player_data.gui.edit_window then
+        -- editing
+        local slot = get_editing_slot(player_data)
+
+        if #(e.entities) > 0 then
+            --- @type LuaEntity
+            local entity = e.entities[1]
+            slot.entity = entity
+            slot.sprite = "entity/" .. entity.name
+            slot.caption = entity.entity_label
+        else
+            local position = e.area.left_top
+            slot.position = position
+            slot.sprite = "item/radar"
+            slot.caption = "x=" .. position.x .. " y=" .. position.y
+        end
+        refresh_edit_window(player)
+    else
+        -- create new
+        add_shortcut(game.players[player_index], e)
+    end
+end
+
+script.on_event(defines.events.on_player_selected_area, function(e)
+    on_selection(e)
+end)
+
+script.on_event(defines.events.on_player_alt_selected_area, function(e)
+    on_selection(e)
 end)
 
 
@@ -346,7 +503,6 @@ script.on_event(defines.events.on_player_removed, function(event)
 end)
 
 script.on_init(function()
-    game.print("on_init")
     for _, player in pairs(game.players) do
         init_player(player)
     end
--- data.lua
+++ data.lua
@@ -1,4 +1,3 @@
-
 local controller = table.deepcopy(data.raw["spidertron-remote"]["spidertron-remote"])
 
 controller.name = "cls-spidertron-remote-oic"
@@ -6,23 +5,26 @@ controller.flags = controller.flags or {}
 table.insert(controller.flags, "hidden")
 table.insert(controller.flags, "only-in-cursor")
 
-data:extend({controller,
+data:extend({ controller,
     {
         type = "selection-tool",
         name = "cls-location-selection-tool",
         icon = "__core__/graphics/clone-icon.png",
         icon_size = 32,
-        flags = {"hidden", "not-stackable", "only-in-cursor"},
+        flags = { "hidden", "not-stackable", "only-in-cursor" },
         stack_size = 1,
 
         selection_cursor_box_type = "entity",
         selection_color = { r = 0, g = 1, b = 0 },
-        selection_mode = {"any-entity", "friend"},
+        selection_mode = { "any-entity", "friend" },
+        entity_type_filters = { "spider-leg" },
+        entity_filter_mode = "blacklist",
 
         alt_selection_cursor_box_type = "entity",
         alt_selection_color = { r = 0, g = 0, b = 1 },
-        alt_selection_mode = {"any-entity", "friend"},
-        alt_entity_type_filters = {"car", "spider-vehicle", "locomotive", "cargo-wagon", "fluid-wagon", "artillery-wagon"},
+        alt_selection_mode = { "any-entity", "friend" },
+        alt_entity_type_filters = { "car", "spider-vehicle",
+            "locomotive", "cargo-wagon", "fluid-wagon", "artillery-wagon" },
         alt_entity_filter_mode = "whitelist",
     }
 })
